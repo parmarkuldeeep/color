4524ebce992c296010016889e93bbf482437511b
parmarkuldeeep <81968141+parmarkuldeeep@users.noreply.github.com>
2021-04-14 14:50:58 +0530

    Add files via upload

---
+++ background.js
@@ -0,0 +1,20 @@
+var css= document.querySelector("h3");
+var color1= document.querySelector(".color1");
+var color2= document.querySelector(".color2");
+var body= document.getElementById("gradient");
+
+function SetGradient() {
+	body.style.background = 
+	"Linear-gradient(to right, "  
+	+ color1.value 
+	+ ", " 
+	+ color2.value 
+	+ ")";
+
+	css.textContent = body.style.background + ";";
+
+}
+
+color1.addEventListener("input", SetGradient);
+
+color2.addEventListener("input", SetGradient);
